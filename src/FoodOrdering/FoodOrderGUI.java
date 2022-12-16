e1b407cbcc073d55e46774d26fd811c065f82db3
Liden <lninyow@gmail.com>
2022-12-16 12:18:47 +0800

    Added FoodOrder main function and food_order method to calculate the amount of PHP

--- src/FoodOrdering/FoodOrderGUI.java
+++ src/FoodOrdering/FoodOrderGUI.java
@@ -1,8 +1,12 @@
 package FoodOrdering;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.List;
 
-public class FoodOrderGUI {
+public class FoodOrderGUI extends JFrame{
     private JPanel panel1;
     private JCheckBox cPizza;
     private JRadioButton rbNone;
@@ -15,4 +19,43 @@ public class FoodOrderGUI {
     private JRadioButton rb5;
     private JRadioButton rb10;
     private JRadioButton rb15;
+
+    public FoodOrderGUI(){
+        setTitle("Food Ordering System");
+
+
+        btnOrder.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                food_order();
+            }
+        });
+    }
+    public static void main(String[] args) {
+        FoodOrderGUI app = new FoodOrderGUI();
+        app.setContentPane(app.panel1);
+        app.setSize(550, 700);
+        app.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        app.setVisible(true);
+    }
+
+    public void food_order(){
+        double sum = 0;
+            if(cPizza.isSelected()){
+                sum+= 100;
+            }if(cBurger.isSelected()){
+                sum+=80;
+            }if(cFries.isSelected()){
+                sum+=65;
+            }if(cSoftDrinks.isSelected()){
+                sum+=55;
+            }if(cTea.isSelected()){
+                sum+=50;
+            }if(cSundae.isSelected()){
+                sum+=40;
+            }
+            String nFormat = String.format("%.2f",sum);
+        JOptionPane.showMessageDialog(panel1,"The total price is Php "+ nFormat);
+    }
+
 }
