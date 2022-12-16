8fe984dad9d153fd96567f3eca2b83711fb0c887
Liden <lninyow@gmail.com>
2022-12-16 10:37:14 +0800

    Added operations for SimpleCalc

--- src/SimpleCalc/SimpleCalcGUI.java
+++ src/SimpleCalc/SimpleCalcGUI.java
@@ -1,6 +1,8 @@
 package SimpleCalc;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class SimpleCalcGUI extends JFrame{
     private JPanel panel1;
@@ -12,13 +14,40 @@ public class SimpleCalcGUI extends JFrame{
 
     public SimpleCalcGUI(){
         setTitle("Simple Calculator");
+        lblResult.setEditable(false);
+        lblResult.setFocusable(false);
+
+        btnCompute.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                operations();
+            }
+        });
     }
 
     public static void main(String[] args) {
         SimpleCalcGUI app = new SimpleCalcGUI();
         app.setContentPane(app.panel1);
-        app.setSize(500, 700);
+        app.setSize(550, 700);
         app.setDefaultCloseOperation(EXIT_ON_CLOSE);
         app.setVisible(true);
     }
+
+    public void operations(){
+        int n1 = Integer.parseInt(tfNumber1.getText());
+        int n2 = Integer.parseInt(tfNumber2.getText());
+        String op = (String) cbOperations.getSelectedItem();
+            int result = 0;
+        if(op.equals("+")) {
+            result = n1 + n2;
+        }else if(op.equals("-")){
+            result = n1-n2;
+        }else if(op.equals("*")){
+            result = n1*n2;
+        }else if(op.equals("/")){
+            result = n1/n2;
+        }
+        lblResult.setText(Integer.toString(result));
+
+        }
 }
